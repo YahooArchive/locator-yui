5219983b4514b5fff354ff1467cb2cea68f868b7
imalberto <imalberto@gmail.com>
2013-10-14 11:43:35 -0700

    Added shifter-test

---
+++ tests/fixtures/metas-parsed-error.js
@@ -0,0 +1,33 @@
+
+/*jslint node:true,nomen:true*/
+/*global YUI*/
+
+YUI.add('metas', function (Y, NAME) {
+    "use strict";
+    Y.applyConfig({
+        // missing a { here
+        groups:
+            app: Y.merge((Y.config.groups && Y.config.groups.app) || {}, {
+                modules: {
+                    foo: {
+                        path: "assets/foo.js",
+                        requires: ["node"]
+                    },
+                    bar: {
+                        path: "bar-hash123.js",
+                        requires: ["io-base", "foo"]
+                    },
+                    baz: {
+                        path: "baz-123.css",
+                        type: "css"
+                    },
+                    xyz: {
+                        path: "xyz.css",
+                        type: "css",
+                        requires: ["baz"]
+                    }
+                }
+            })
+        }
+    });
+}, '0.0.1', {requires: ['loader-base']});
