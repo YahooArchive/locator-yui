39dd55ad812ddba2e00bd6e09006d4ce77bfc818
imalberto <imalberto@gmail.com>
2013-10-18 20:05:44 -0700

    Added examples

---
+++ examples/simple/models/alerts-model.js
@@ -0,0 +1,11 @@
+/*jslint */
+/*global YUI*/
+YUI.add('alerts-model', function (Y, NAME) {
+
+    "use strict";
+
+    Y.namespace('Model').Alerts = Y.Model;
+
+}, '@VERSION@', {
+    requires: ['model']
+});
